6729c5ae1e3e7d9080e7e4718254e37db802309b
Doireann-Ni-Ghrioghair <doireanndoireann@gmail.com>
2021-02-22 15:30:57 +0000

    Set up POST request in formHandler

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,15 +2,40 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let articleUrl = document.getElementById('url').value
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
-}
+
+
+    //POST request
+const postData = async(url = '', data = {})=>{
+    console.log(data);
+    const response = await fetch('http://localhost:8081',{
+        method: 'POST',
+        credentials: 'same-origin',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify(data),
+    });
+
+         try{
+             const newData = awaitresponse.json();
+             console.log(newData);
+             return newData;
+         }catch(error){
+             console.log("error", error);
+         }
+        }
+    }
+
+    postData('/add', {test:'it works'});
+
+    //.then(res => res.json())
+    //.then(function(res) {
+    //    document.getElementById('results').innerHTML = res.message
+   // })
+
 
 export { handleSubmit }
