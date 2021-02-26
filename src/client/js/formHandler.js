6c26bba8a5da9880dc4f5e0cf0e2ecd49e1b4eef
Doireann-Ni-Ghrioghair <doireanndoireann@gmail.com>
2021-02-26 14:09:20 +0000

    Style list

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -33,27 +33,27 @@ fetch('http://localhost:8080/addData',{
             block:"end"
     })
         
-        document.getElementById('confidence').innerHTML = "Feelings of confidence in this text are given a rating of "+ res.confidence +"%.";
+        document.getElementById('confidence').innerHTML = "- Feelings of <em>confidence</em> in this text are given a rating of "+ res.confidence +"%";
         if (res.score_tag === 'N'){
-        document.getElementById('polarity').innerHTML = "Overall, the tone is negative." }
+        document.getElementById('polarity').innerHTML = "- Overall, the tone is <em>negative.</em>" }
         else if(res.score_tag === 'N+'){
-            document.getElementById('polarity').innerHTML = "Overall, the tone is very negative."}
+            document.getElementById('polarity').innerHTML = "- Overall, the tone is very negative."}
         else if(res.score_tag === 'NONE'){
-            document.getElementById('polarity').innerHTML = "No sentiment is detected."}
+            document.getElementById('polarity').innerHTML = "- No sentiment is detected."}
         else if(res.score_tag === 'P+'){
-            document.getElementById('polarity').innerHTML = "Overall, the tone is very positive"}
+            document.getElementById('polarity').innerHTML = "- Overall, the tone is very positive"}
         else if(res.score_tag === 'NEU'){
-            document.getElementById('polarity').innerHTML = "Overall, the tone is neutral."}
+            document.getElementById('polarity').innerHTML = "- Overall, the tone is neutral."}
          else if(res.score_tag === 'P'){
-             document.getElementById('polarity').innerHTML = "Overall, the tone is positive."};
+             document.getElementById('polarity').innerHTML = "- Overall, the tone is positive."};
         if (res.agreement === 'AGREEMENT'){
-            document.getElementById('agreement').innerHTML = "This tone is consistent throughout the text"}
+            document.getElementById('agreement').innerHTML = "- This tone is consistent throughout the text"}
             else if(res.agreement === 'DISAGREEMENT'){
-                document.getElementById('agreement').innerHTML = "However, this tone isn't consistent throughout the text"};
+                document.getElementById('agreement').innerHTML = "- However, this tone isn't consistent throughout the text"};
          if(res.subjectivity === 'OBJECTIVE'){
-             document.getElementById('obj').innerHTML = "Our analysis notes that this article is more objective than subjective."}
+             document.getElementById('obj').innerHTML = "- Our analysis notes that this article is more objective than subjective."}
          else if(res.subjectivity === 'SUBJECTIVE'){
-             document.getElementById('obj').innerHTML = "Our analysis notes that this article is more subjective than objective." }
+             document.getElementById('obj').innerHTML = "- Our analysis notes that this article is more subjective than objective." }
          else if(res.subjectivity === null){
              document.getElementById('obj').innerHTML = ""}; 
          if(res.irony === 'NONIRONIC'){
