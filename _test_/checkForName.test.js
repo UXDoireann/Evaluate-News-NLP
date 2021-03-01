a462f617007367af5e6e253cec965d09b5cf0b83
Doireann-Ni-Ghrioghair <doireanndoireann@gmail.com>
2021-03-01 13:25:18 +0000

    Add checkForName test

---
+++ _test_/checkForName.test.js
@@ -0,0 +1,8 @@
+import{checkForName} from "...src/client/js/nameChecker.js"
+import { checkForName} from ".../src/client/js/nameChecker.js"
+
+describe("Testing to make sure input is url",()=>{
+    test("Testing the checkForName() function", ()=>{
+       expect(checkForName(hello).toBe(false));
+        })})
+        
