df88e928df89d291afb432176a54f57f657f541e
Doireann-Ni-Ghrioghair <doireanndoireann@gmail.com>
2021-03-01 21:51:15 +0000

    Remove alert

--- src/client/index.js
+++ src/client/index.js
@@ -4,8 +4,7 @@ import { handleSubmit } from './js/formHandler'
 
 console.log(checkForName);
 
-alert("I EXIST")
-console.log("CHANGE!!");
+
 
 import './styles/resets.scss'
 import './styles/base.scss'
@@ -22,8 +21,4 @@ export {
 
    import './styles/logo.jpg'
 
-  /* if (window.innerWidth >= 600) {
-    import('./styles/header-desktop.scss');
-}else if(window.innerWidth<=500){
-    import('./styles/header-mobile.scss');
-}*/
+ 
